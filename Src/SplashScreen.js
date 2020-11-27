12f430624a423c19443b9987afffde6ac02220a9
KAMPETE <peterkampete01@gmail.com>
2020-11-27 07:26:43 -0500

    First commit

---
+++ Src/SplashScreen.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const SplashScreen = () => {
+  return (
+    <View>
+      <Text>SplashScreen</Text>
+    </View>
+  );
+};
